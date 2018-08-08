14e184d2a6134ad6ba3c70dd5fd59d7919703204
hopelevin <hopealevin@gmail.com>
2018-08-08 13:56:18 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,19 +1,19 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "raspberry"
 
 puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = your_favorite_word.upcase
 
 puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING"
+phrase = "IM NOT SHOUTING".downcase
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
@@ -23,13 +23,13 @@ puts lowercased_phrase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length # Put a . after big_word and use a String method.
 
 puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom"
 
 new_letter_count = bigger_word.size
 
@@ -39,13 +39,17 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+puts "proper_sentence #{i really like programming}
 
 # Challenge 7: String Switcheroo
+
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+"A" = "O" 
+"a" = "o" 
+
+sentence = #{A}stounding #{a}#{a}rdv#{a}rks, #{a}rgu#{a}bly #{a}n #{a}ncient #{a}nim#{a}l, #{a}lw#{a}ys #{a}te #{a}pples #{a}nd #{a}corns #{a}nd #{a}rtichokes #{a}ll #{a}round #{A}thens, #{a}m#{a}zing!"
 
 puts sentence
